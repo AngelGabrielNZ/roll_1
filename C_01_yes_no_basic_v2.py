a6658293f0e45366d8540213b43b26c66eb04a28
AngelGabrielNZ <andersene3512@masseyhigh.school.nz>
2025-02-11 12:27:44 +1300

    Working instructions

--- C_01_yes_no_basic_v2.py
+++ C_01_yes_no_basic_v2.py
@@ -1,7 +1,8 @@
 # functions go here
 
-
 def yes_no(question):
+    """checks the user response to a question is yes / no (y/n), returns 'yes' or 'no' """
+
     while True:
 
         response = input(question).lower()
@@ -15,9 +16,25 @@ def yes_no(question):
             print("please enter yes / no")
 
 
+def instructions():
+    """Prints instructions"""
+
+    print("""
+**** Instructions ****
+
+Roll the dice and try to win!
+    """)
+
+
 # Main routine
 
-want_instructions = yes_no("Do you want to see the instructions? ")
-want_coffee = yes_no("Do you want coffee? ")
 
-print("we done")
+# ask the user if they want instructions (check they say yes / no)
+want_instructions = yes_no("Do you want to see the instructions? ")
+
+# display the instruction if user wants to see them...
+if want_instructions == "yes":
+    instructions()
+
+print()
+print("Program continues")
