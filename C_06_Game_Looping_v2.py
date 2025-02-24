a18ef7493b43dd3617ca08752cbb6a07bc5c09e5
AngelGabrielNZ <andersene3512@masseyhigh.school.nz>
2025-02-25 09:44:59 +1300

    working looping componets (basic)

---
+++ C_06_Game_Looping_v2.py
@@ -0,0 +1,26 @@
+# At the start of the game, the computer / user score are both zero
+comp_score = 0
+user_score = 0
+
+game_goal = int(input("Game Goal"))  # should be a function call!
+
+# play multiple rounds until a winner has been found
+while comp_score < game_goal and user_score < game_goal:
+    # Start of round Loop
+    # For testng purposes, ask the user what the points for the user / computer were
+    comp_points = int(input("Enter the computer points at the end of the round:"))
+    user_points = int(input("Enter the user points at the end of round"))
+    # Outside rounds Loop - Update score with round points, only add to the score of the
+    comp_score += comp_points
+    user_score += user_points
+
+    # show overall score (add this to rounds loop)
+    print("*** Game Update ***")  # replace with call statement generator
+    print(f"User Score: {user_score} | computer score {comp_score}")
+
+# end of entire game, output final results
+print()
+if user_score > comp_score:
+    print("The user won")  # replace this with statement generator call
+else:
+    print("The computer won")
